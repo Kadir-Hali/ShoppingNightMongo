31a68123538707f692e81167488c2aeb925e1654
Kadir-Hali <kadirhali07@gmail.com>
2025-05-14 15:00:21 +0300

    14.05.25

--- ShoppingNightMongo/Settings/IDatabaseSettings.cs
+++ ShoppingNightMongo/Settings/IDatabaseSettings.cs
@@ -2,5 +2,9 @@
 {
     public interface IDatabaseSettings
     {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CategoryCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
     }
 }
