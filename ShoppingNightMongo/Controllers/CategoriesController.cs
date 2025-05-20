cff779cc53f5cdb8f62ef46fe5ad8bfdd56796d0
Kadir-Hali <kadirhali07@gmail.com>
2025-05-20 20:54:51 +0300

    20.05.25-1

---
+++ ShoppingNightMongo/Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingNightMongo.Dtos.CategoryDtos;
+using ShoppingNightMongo.Services.CategoryServices;
+
+namespace ShoppingNightMongo.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _categoryService.GetAllCategoryAsync();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CretaeCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            await _categoryService.CreateCategoryAsync(createCategoryDto);
+            return RedirectToAction("CategoryList");
+        }
+
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            await _categoryService.DeleteCategoryAsync(id);
+            return RedirectToAction("CategoryList");
+        }
+    }
+}
